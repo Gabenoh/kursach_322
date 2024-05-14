f11489351e9ad24a324cf4e49617a18ff149d972
gabenoh <mixabuchko@gmail.com>
2024-05-14 10:56:45 +0300

    add more func

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart
+from aiogram.filters.command import Command
 from utils import TOKEN
 import aiohttp
 import asyncio
 
-
 app = Flask(__name__)
-TOKEN = TOKEN
-
 dp = Dispatcher()
 
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
+    data = {'message': 'Привіт, це мій перший API!'}
+    return jsonify(data)
+
+
+@app.route('/data/', methods=['GET'])
+def data_get():
     data = {'message': 'Привіт, це мій перший API!'}
     return jsonify(data)
 
@@ -33,19 +38,50 @@ async def get_card(name):
         print(e)
 
 
-async def main_bot() -> None:
+@dp.message(CommandStart())
+async def start_command(message: types.Message):
+    await message.reply(f"Привіт {message.from_user.first_name}, я буду твоїм помічником у грі Hearthstone")
+
+
+# Функція, яка буде викликатися після команди /search
+async def search(message: types.Message):
+    # Отримуємо текст команди та аргументи
+    command, *args = message.text.split(' ')
+    search_query = ' '.join(args)
+
+    # Виконуємо вашу логіку пошуку тут
+    print('шукаєм', search_query)
+    await message.reply(f"Ви шукаєте: {search_query}")
+    info = await get_card(search_query)
+    print(info)
+    try:
+        return info[0]['img']
+    except KeyError:
+        await message.reply(f'Карти {search_query} не знайдено')
+
+
+@dp.message(Command('пошук'))
+async def search_command(message: types.Message):
+    photo_url = await search(message)
+    try:
+        await message.reply_photo(photo_url)
+    except:
+        pass
+
+
+async def bot_run() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN)
     # And the run events dispatching
     await dp.start_polling(bot)
 
 
-async def main_aio():
-    data = await get_card('Лирой Дженкинс')
-    print(data)
+# async def main_aio():
+#     data = await get_card('Лирой Дженкинс')
+#     print(data)
 
 
 if __name__ == '__main__':
-    # asyncio.run(main_bot())
-    asyncio.run(main_aio())
+    # asyncio.run(main_aio())
+    asyncio.run(bot_run())
     app.run(debug=True)
